252199e81243c79f1f6730cb415294b08b0e00e9
doinel1a <doinel1atanasiu@gmail.com>
2024-01-16 15:21:50 +0100

    refactor(dapp): remove unused components

--- dapp/src/app.tsx
+++ dapp/src/app.tsx
@@ -2,9 +2,7 @@ import React from 'react';
 
 import { BrowserRouter } from 'react-router-dom';
 
-import Counter from './components/counter';
 import Footer from './components/footer';
-import GithubCorner from './components/github-corner';
 import Navbar from './components/navbar';
 import ThemeProvider from './components/ui/theme/provider';
 import EStorageKeys from './constants/keys';
@@ -16,11 +14,7 @@ function App() {
         <Navbar />
 
         <main className='flex h-full flex-col items-center justify-center'>
-          <GithubCorner
-            title='Get started on GitHub'
-            url='https://github.com/doinel1a/vite-react-ts-shadcn-ui'
-          />
-          <Counter />
+          <h1 className='text-4xl'>GhoStream | Hackathon LFGHO</h1>
         </main>
 
         <Footer />
