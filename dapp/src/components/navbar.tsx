b6dceb78d602c535d04f0c51b9d95dd2bd70b439
doinel1a <doinel1atanasiu@gmail.com>
2024-01-16 17:19:42 +0100

    feat(dapp): implement wallet button

--- dapp/src/components/navbar.tsx
+++ dapp/src/components/navbar.tsx
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 
+import { Skeleton } from './ui/skeleton';
 import ThemeToggle from './ui/theme-toggle';
+
+const WalletButton = React.lazy(() => import('./wallet-button'));
 
 export default function Navbar() {
   return (
     <header className='flex h-16 w-full items-center justify-between border-b border-border px-5'>
       <span className='text-lg font-black'>Template</span>
 
-      <ThemeToggle />
+      <div className='flex gap-x-5'>
+        <Suspense fallback={<Skeleton className='h-10 w-40' />}>
+          <WalletButton className='w-40' />
+        </Suspense>
+
+        <ThemeToggle />
+      </div>
     </header>
   );
 }
