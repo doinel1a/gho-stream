b56255853d3487d3f5fb0d0a16573ef661d1df09
urataps <alexeluca.spataru@gmail.com>
2024-01-17 19:58:27 +0200

    feat: aave integration

--- contracts/test/Test.t.sol
+++ contracts/test/Test.t.sol
@@ -3,12 +3,12 @@ pragma solidity 0.8.23;
 
 import { Test } from "forge-std/Test.sol";
 import { ISablierV2LockupLinear } from "@sablier/interfaces/ISablierV2LockupLinear.sol";
-import { GhoStreamFacilitator } from "../src/GhoStreamFacilitator.sol";
+// import { GhoStreamAToken } from "src/GhoStreamAToken.sol";
 
 contract BaseTest is Test {
-    GhoStreamFacilitator public ghoStreamFacilitator;
+    // GhoStreamAToken public ghoStreamAToken;
 
     function setUp() public {
-        ghoStreamFacilitator = new GhoStreamFacilitator();
+        // ghoStreamAToken = new GhoStreamAToken();
     }
 }
