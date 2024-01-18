f1fc95cec0556e6ddcc792604f99b6cfa286ca18
doinel1a <doinel1atanasiu@gmail.com>
2024-01-18 16:12:19 +0100

    refactor(dapp/supplied-assets-section): remove approve transaction in withdraw modal

--- dapp/src/components/sections/widgets/withdraw-assets-dialog.tsx
+++ dapp/src/components/sections/widgets/withdraw-assets-dialog.tsx
@@ -1,13 +1,7 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 
 import type { IToken } from '@/interfaces/token';
-import type {
-  IApproveTransactionAction,
-  TApproveTransactionState
-} from '@/reducers/approve-transaction';
 
-import ErrorBanner from '@/components/error-banner';
-import LoadingButton from '@/components/loading-button';
 import { Button } from '@/components/ui/button';
 import {
   Dialog,
@@ -16,44 +10,19 @@ import {
   DialogTitle,
   DialogTrigger
 } from '@/components/ui/dialog';
-import EReducerState from '@/constants/reducer-state';
 
 import AmountInput from './amount-input';
 
 interface IWithdrawAssetsSection {
   token: IToken;
-  approveTransactionState: TApproveTransactionState;
-  onApproveClick(contractName: string, amount: string): Promise<void>;
   onWithdrawClick: () => void;
-  dispatchApproveTransaction: React.Dispatch<IApproveTransactionAction>;
 }
 
-export default function WithdrawAssetsSection({
-  token,
-  approveTransactionState,
-  onApproveClick,
-  onWithdrawClick,
-  dispatchApproveTransaction
-}: IWithdrawAssetsSection) {
+export default function WithdrawAssetsSection({ token, onWithdrawClick }: IWithdrawAssetsSection) {
   const [isDialogOpen, setIsDialogOpen] = useState(false);
   const [amount, setAmount] = useState('');
 
   const isInputValid = amount !== '' && amount !== '0';
-
-  const errorMessage = approveTransactionState.errorCode
-    ? approveTransactionState.errorCode === 4001
-      ? 'You cancelled the transaction.'
-      : 'Error with your transaction.'
-    : 'Error with your transaction.';
-
-  useEffect(() => {
-    if (!isDialogOpen) {
-      dispatchApproveTransaction({
-        state: EReducerState.reset,
-        payload: undefined
-      });
-    }
-  }, [isDialogOpen, dispatchApproveTransaction]);
 
   return (
     <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
@@ -70,24 +39,8 @@ export default function WithdrawAssetsSection({
           <AmountInput id='withdraw-assets' token={token} amount={amount} setAmount={setAmount} />
         </div>
 
-        {approveTransactionState.isError && <ErrorBanner>{errorMessage}</ErrorBanner>}
-
         <div className='flex w-full flex-col gap-y-2.5'>
-          {isInputValid && (
-            <LoadingButton
-              variant='secondary'
-              isLoading={approveTransactionState.isLoading}
-              loadingContent='Approving'
-              defaultContent='Approve to continue'
-              disabled={!isInputValid || approveTransactionState.isLoading}
-              onClick={() => onApproveClick(token.name, amount)}
-            />
-          )}
-
-          <Button
-            disabled={!isInputValid || !approveTransactionState.isSuccess}
-            onClick={onWithdrawClick}
-          >
+          <Button disabled={!isInputValid} onClick={onWithdrawClick}>
             {isInputValid ? <span>Withdraw {token.name}</span> : <span>Enter an amount</span>}
           </Button>
         </div>
