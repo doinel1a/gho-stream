cf44ea4e9df34c7518d7c981621c6a3d88f4c70e
urataps <alexeluca.spataru@gmail.com>
2024-01-18 16:24:48 +0200

    feat: add borrow functionality

--- contracts/src/AaveMiniMarket.sol
+++ contracts/src/AaveMiniMarket.sol
@@ -1,17 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.23;
 
+import { EnumerableSet } from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import { IAToken } from "@aave/core-v3/contracts/interfaces/IAToken.sol";
+
+import { ISablierV2LockupLinear } from "@sablier/interfaces/ISablierV2LockupLinear.sol";
+import { Broker, LockupLinear } from "@sablier/types/DataTypes.sol";
+import { ud60x18 } from "@prb/math/src/UD60x18.sol";
+
+import { IGhoToken } from "./interfaces/IGhoToken.sol";
 
 /**
  * @dev Excessively simplified version of the Aave Pool contract.
  */
 contract AaveMiniMarket {
+    using EnumerableSet for EnumerableSet.UintSet;
+
+    // Sepolia Addresses
+    IGhoToken public immutable GHO = IGhoToken(0xc4bF5CbDaBE595361438F8c6a187bDc330539c60);
+    ISablierV2LockupLinear public immutable SABLIER_LOCKUP_LINEAR =
+        ISablierV2LockupLinear(0x7a43F8a888fa15e68C103E18b0439Eb1e98E4301);
+
     mapping(IERC20 token => IAToken aToken) public aTokenOf;
 
+    // Streaming
+    mapping(address borrower => EnumerableSet.UintSet streamIds) private _borrowerStreamIds;
+    mapping(uint256 streamId => address borrower) private _borrowerOfStreamId;
+
     constructor(IERC20[] memory tokens, IAToken[] memory aTokens) {
-        require(tokens.length == aTokens.length, "AaveMiniMarket: length mismatch");
+        require(tokens.length == aTokens.length, "Length mismatch");
 
         for (uint256 i = 0; i < tokens.length; i++) {
             aTokenOf[tokens[i]] = aTokens[i];
@@ -26,5 +44,57 @@ contract AaveMiniMarket {
     function withdraw(IERC20 token, uint256 amount) external {
         aTokenOf[token].burn(msg.sender, msg.sender, amount, 0);
         token.transfer(msg.sender, amount);
+    }
+
+    function borrowGhoThroughStream(uint128 amount, uint40 streamDuration, address streamRecipient) external {
+        uint256 nextStreamId = SABLIER_LOCKUP_LINEAR.nextStreamId();
+
+        _borrowerStreamIds[msg.sender].add(nextStreamId);
+        _borrowerOfStreamId[nextStreamId] = msg.sender;
+
+        GHO.mint(address(this), amount);
+        GHO.approve(address(SABLIER_LOCKUP_LINEAR), amount);
+        uint256 streamId = _createGhoStream(amount, streamDuration, streamRecipient);
+
+        require(streamId == nextStreamId, "StreamId mismatch");
+    }
+
+    function cancelGhoStream(uint256 streamId) external {
+        require(_borrowerOfStreamId[streamId] == msg.sender, "Not borrower");
+
+        SABLIER_LOCKUP_LINEAR.cancel(streamId);
+        LockupLinear.Stream memory stream = SABLIER_LOCKUP_LINEAR.getStream(streamId);
+
+        GHO.burn(stream.amounts.refunded);
+    }
+
+    function getBorrowerStreamIds(address borrower) external view returns (uint256[] memory streamIds) {
+        streamIds = _borrowerStreamIds[borrower].values();
+    }
+
+    function _createGhoStream(
+        uint128 totalAmount,
+        uint40 duration,
+        address recipient
+    )
+        internal
+        returns (uint256 streamId)
+    {
+        // Approve the Sablier contract to spend GHO
+        GHO.approve(address(SABLIER_LOCKUP_LINEAR), totalAmount);
+
+        // Declare the params struct
+        LockupLinear.CreateWithDurations memory params = LockupLinear.CreateWithDurations({
+            sender: address(this),
+            recipient: recipient,
+            totalAmount: totalAmount,
+            asset: GHO,
+            cancelable: true, // Whether the stream will be cancelable or not
+            transferable: true, // Whether the stream will be transferable or not
+            durations: LockupLinear.Durations({ cliff: 0, total: duration }),
+            broker: Broker(address(0), ud60x18(0)) // TODO: send funds to gho treasury
+         });
+
+        streamId = SABLIER_LOCKUP_LINEAR.createWithDurations(params);
     }
 }
--- contracts/test/Test.t.sol
+++ contracts/test/Test.t.sol
@@ -3,12 +3,27 @@ pragma solidity 0.8.23;
 
 import { Test } from "forge-std/Test.sol";
 import { ISablierV2LockupLinear } from "@sablier/interfaces/ISablierV2LockupLinear.sol";
-// import { GhoStreamAToken } from "src/GhoStreamAToken.sol";
+import { AaveMiniMarket } from "src/AaveMiniMarket.sol";
+import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { IAToken } from "@aave/core-v3/contracts/interfaces/IAToken.sol";
 
 contract BaseTest is Test {
-    // GhoStreamAToken public ghoStreamAToken;
+    AaveMiniMarket public aaveMiniMarket;
+    IERC20 public WETH = IERC20(0xC558DBdd856501FCd9aaF1E62eae57A9F0629a3c);
+    IERC20 public DAI = IERC20(0xFF34B3d4Aee8ddCd6F9AFFFB6Fe49bD371b8a357);
+    IERC20 public USDC = IERC20(0x94a9D9AC8a22534E3FaCa9F4e7F2E2cf85d5E4C8);
+    IERC20[] public tokens;
+    IAToken[] public aTokens;
 
     function setUp() public {
-        // ghoStreamAToken = new GhoStreamAToken();
+        tokens.push(WETH);
+        tokens.push(DAI);
+        tokens.push(USDC);
+
+        aTokens.push(IAToken(0xaC775C0b34c50Ba68bBC0e4F3c9aCCaf34123eda));
+        aTokens.push(IAToken(0x72CB9080841acB75E5AB9d83E5F78a3d20326e6A));
+        aTokens.push(IAToken(0x915790Fe8cc10Acf844CB77F8DC3299d4E3be78a));
+
+        aaveMiniMarket = new AaveMiniMarket(tokens, aTokens);
     }
 }
