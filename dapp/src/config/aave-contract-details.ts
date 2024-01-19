6ace79fd81c64858ca0bfebbab92317589438f9e
doinel1a <doinel1atanasiu@gmail.com>
2024-01-19 10:09:22 +0100

    refactor(dapp/config): update aave contract details to last deployment

--- dapp/src/config/aave-contract-details.ts
+++ dapp/src/config/aave-contract-details.ts
@@ -1,7 +1,7 @@
 import aaveMiniMarketAbi from '../abis/AaveMiniMarket.json';
 
 const aaveContractDetails = {
-  address: '0x832Cd0ff6dC17A5055ca7c11aFa08B32f8E7202F',
+  address: '0x86454EF8dB62Fc2cf9C54fe33E843199074EBeBC',
   artifacts: aaveMiniMarketAbi
 };
 
