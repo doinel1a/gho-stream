44f1ea55555f73a33dd15000de61605a67f58e30
doinel1a <doinel1atanasiu@gmail.com>
2024-01-19 19:28:56 +0100

    refactor(dapp/navbar): update css

--- dapp/src/components/navbar.tsx
+++ dapp/src/components/navbar.tsx
@@ -7,17 +7,19 @@ const WalletButton = React.lazy(() => import('./wallet-button'));
 
 export default function Navbar() {
   return (
-    <nav className='flex h-16 w-full items-center justify-between border-b border-border px-5'>
-      <span className='text-lg font-black'>Template</span>
+    <nav className='z-10 h-16 w-full border-b border-border px-5'>
+      <div className='mx-auto flex h-full w-full max-w-7xl items-center justify-between'>
+        <span className='text-lg font-black'>Template</span>
 
-      <div className='flex gap-x-5'>
-        <Suspense fallback={<Skeleton className='h-10 w-40' />}>
-          <WalletButton className='w-40' />
-        </Suspense>
+        <div className='flex gap-x-5'>
+          <Suspense fallback={<Skeleton className='h-10 w-40' />}>
+            <WalletButton className='w-40' />
+          </Suspense>
 
-        <Suspense fallback={<Skeleton className='h-10 w-10' />}>
-          <ThemeToggle />
-        </Suspense>
+          <Suspense fallback={<Skeleton className='h-10 w-10' />}>
+            <ThemeToggle />
+          </Suspense>
+        </div>
       </div>
     </nav>
   );
